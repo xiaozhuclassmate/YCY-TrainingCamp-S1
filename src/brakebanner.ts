0afa20b5584e7b02f14f676b7d5a4c04431a9da1
xiaozhuclassmate <zx15727547121@163.com>
2022-07-15 14:07:08 +0800

    complete

--- src/brakebanner.ts
+++ src/brakebanner.ts
@@ -1,31 +1,35 @@
 import gsap from "gsap"
 import { Application, Container, Graphics, Loader, Sprite } from "pixi.js"
 
-enum Images {
-  BRAKE_BIKE = "brake_bike.png",
-  BRAKE_HANDLERBAR = "brake_handlerbar.png",
-  BRAKE_LEVER = "brake_lever.png",
-  BTN_CIRCLE = "btn_circle.png",
-  BTN = "btn.png",
-}
+  enum Images {
+    BRAKE_BIKE = "brake_bike.png",
+    BRAKE_HANDLERBAR = "brake_handlerbar.png",
+    BRAKE_LEVER = "brake_lever.png",
+    BTN_CIRCLE = "btn_circle.png",
+    BTN = "btn.png",
+  }
 
 export class BrakeBanner {
   // pixi 实例
   private app: Application
   // 加载器
   private loader: Loader
+  // banner 容器
+  private breakBannerContainer: Container = new Container()
   // 所有的雨滴
-  public raindrops: any[] = []
-  // 车身
-  public brake_bike: any = null
+  private raindrops: any[] = []
+  //雨滴数量
+  private raindropCount: number = 20
   // 车容器
-  public bycyleContainer: Container | null = null
+  private bycyleContainer: Container = new Container()
   // 刹车
-  public brake_lever: Sprite | null = null
+  private brake_lever: Sprite | null = null
+  // 车身
+  private brake_bike: Sprite | null = null
   // 按钮容器
-  private btnContainer: Container | null = null
+  private btnContainer: Container = new Container()
   // 车速
-  public speed: number = 0
+  private speed: number = 0
   // 缩放大小
   private scale: number = 0.6
   //循环方法
@@ -42,8 +46,6 @@ export class BrakeBanner {
     this.loader = new Loader()
 
     this._init()
-
-    window.addEventListener("resize", this.resize.bind(this))
   }
 
   private async _init() {
@@ -59,14 +61,21 @@ export class BrakeBanner {
     this._initBtn()
     // 添加事件
     this._addEvents()
+    
+    this.breakBannerContainer.scale.x = this.breakBannerContainer.scale.y = this.scale
+    this.app.stage.addChild(this.breakBannerContainer)
+
+    // resize
+    this._resize()
   }
 
   private _addEvents() {
     // 按下按钮时
-    this.btnContainer!.on("pointerdown", () => {
+    this.btnContainer.on("pointerdown", () => {
       // 降低车身透明度
       gsap.to(this.brake_bike, { alpha: 1, duration: 0.3 })
-      // 改变刹车调度，形成刹车动画
+
+      // 改变刹车角度，形成刹车动画
       gsap.to(this.brake_lever, {
         rotation: (-35 * Math.PI) / 180,
         duration: 0.1,
@@ -74,19 +83,20 @@ export class BrakeBanner {
       })
       // 将车容器偏移
       gsap.to(this.bycyleContainer, {
-        y: this.bycyleContainer!.height * 0.02,
-        duration: 0.2,
+        y: this.bycyleContainer.height * 0.1,
+        duration: 0.1,
         ease: "easeInOut",
       })
 
       // 隐藏按钮
       gsap.to(this.btnContainer, { alpha: 0, duration: 0.5 })
 
+      // 停止粒子动画
       this._raindropsPause()
     })
 
     // 松开按钮时
-    this.btnContainer!.on("pointerup", () => {
+    this.btnContainer.on("pointerup", () => {
       // 恢复刹车角度
       gsap.to(this.brake_lever, {
         rotation: (-20 * Math.PI) / 180,
@@ -94,12 +104,12 @@ export class BrakeBanner {
         ease: "easeInOut",
       })
       // 恢复车身透明度
-      gsap.to(this.brake_bike, { alpha: 0.3, duration: 0.3 })
+      gsap.to(this.brake_bike, { alpha: 0.3, duration: 0.2 })
 
       // 恢复车容器偏移
       gsap.to(this.bycyleContainer, {
-        x: window.innerWidth - this.bycyleContainer!.width,
-        y: window.innerHeight - this.bycyleContainer!.height,
+        x: 0,
+        y: 0,
         duration: 0.8,
         ease: "easeInOut",
       })
@@ -126,11 +136,11 @@ export class BrakeBanner {
     })
   }
 
-  public _raindropsRun() {
+  private _raindropsRun() {
     gsap.ticker.add(this.loop)
   }
 
-  public _raindropsPause() {
+  private _raindropsPause() {
     gsap.ticker.remove(this.loop)
 
     this.speed = 0
@@ -147,7 +157,7 @@ export class BrakeBanner {
 
     const colors = [0x34495e, 0xffe720]
 
-    new Array(30).fill(0).forEach((v) => {
+    for(let i = 0; i < this.raindropCount; i++) {
       const gr = new Graphics()
 
       const grItem = {
@@ -160,32 +170,35 @@ export class BrakeBanner {
       gr.y = grItem.y
 
       gr.beginFill(colors[Math.floor(Math.random() * colors.length)])
-      gr.drawCircle(0, 0, 6)
-      gr.endFill()
+      gr.drawCircle(0, 0, 6).endFill()
 
       raindropContainer.addChild(gr)
 
       raindropContainer.pivot.x = window.innerWidth / 2 - 1000
       raindropContainer.pivot.y = window.innerHeight / 2
-
       raindropContainer.rotation = (35 * Math.PI) / 180
 
       this.raindrops.push(grItem)
-    })
+    }
 
     this.app.stage.addChild(raindropContainer)
   }
 
   private _initBtn() {
-    const btnContainer = (this.btnContainer = new Container())
+    const btnContainer = this.btnContainer
 
-    const btnCircle = new Sprite(
-      this.loader.resources[Images.BTN_CIRCLE].texture
-    )
+    const btn = new Sprite(this.loader.resources[Images.BTN].texture)
+    const btnCircle = new Sprite(this.loader.resources[Images.BTN_CIRCLE].texture)
+
     btnCircle.pivot.x = btnCircle.pivot.y = btnCircle.width / 2
-    btnContainer.addChild(btnCircle)
-
     btnCircle.scale.x = btnCircle.scale.y = 0.8
+    
+    btn.pivot.x = btn.pivot.y = btn.width / 2
+    btnContainer.x = 600
+    btnContainer.y = 700
+
+    btnContainer.buttonMode = true
+    btnContainer.interactive = true
 
     gsap.to(btnCircle.scale, {
       x: 1.1,
@@ -195,21 +208,11 @@ export class BrakeBanner {
       ease: "easeInOut",
     })
 
-    const btn = new Sprite(this.loader.resources[Images.BTN].texture)
-    btn.pivot.x = btn.pivot.y = btn.width / 2
     btnContainer.addChild(btn)
-    btnContainer.x = 200
-
-    btnContainer.scale.x = btnContainer.scale.y = this.scale
-    btnContainer.x = window.innerWidth * 0.55
-    btnContainer.y = window.innerHeight * 0.35
-
-    btnContainer.buttonMode = true
-    btnContainer.interactive = true
-
-    this.app.stage.addChild(btnContainer)
+    btnContainer.addChild(btnCircle)
+    this.breakBannerContainer.addChild(btnContainer)
   }
-
+ 
   private async _loadResources() {
     const getLoaderArgs = (name: string): [string, string] => [
       name,
@@ -226,78 +229,42 @@ export class BrakeBanner {
   }
 
   private _initBycycle() {
-    const bycyleContainer = (this.bycyleContainer = new Container())
+    const bycyleContainer = this.bycyleContainer
 
-    // 初始化车身
-    this._initBrakeBike(bycyleContainer)
-    // 初始化刹车
-    this._initBrakeLever(bycyleContainer)
-    // 初始化车头
-    this._initBrakeHandlerbar(bycyleContainer)
-
-    // 让车一直处于画面右下角
-    const setBycyclePosition = () => {
-      bycyleContainer.x = window.innerWidth - bycyleContainer.width
-      bycyleContainer.y = window.innerHeight - bycyleContainer.height
-    }
-    setBycyclePosition()
-    window.addEventListener("resize", setBycyclePosition)
-
-    // 将车容器添加到画布中
-    this.app.stage.addChild(bycyleContainer)
-  }
-
-  // 车身
-  private _initBrakeBike(bycyleContainer: Container) {
-    // 获取车身图片
-    const brake_bike = (this.brake_bike = new Sprite(
-      this.loader.resources["brake_bike.png"].texture
-    ))
-
-    //缩放大小
-    brake_bike.scale.x = brake_bike.scale.y = this.scale
-
+    // 获取资源
+    const brake_bike = this.brake_bike = new Sprite(this.loader.resources["brake_bike.png"].texture)
+    const brake_lever = this.brake_lever = new Sprite(this.loader.resources["brake_lever.png"].texture)
+    const brake_handlerbar = new Sprite(this.loader.resources["brake_handlerbar.png"].texture)
     // 降低透明度
     brake_bike.alpha = 0.3
-
-    //将车身添加到容器中
-    bycyleContainer.addChild(brake_bike)
-  }
-
-  // 车头
-  private _initBrakeHandlerbar(bycyleContainer: Container) {
-    const brake_handlerbar = new Sprite(
-      this.loader.resources["brake_handlerbar.png"].texture
-    )
-
-    brake_handlerbar.scale.x = brake_handlerbar.scale.y = this.scale
-
-    bycyleContainer.addChild(brake_handlerbar)
-  }
-
-  // 刹车
-  private _initBrakeLever(bycyleContainer: Container) {
-    const brake_lever = (this.brake_lever = new Sprite(
-      this.loader.resources["brake_lever.png"].texture
-    ))
-
-    brake_lever.scale.x = brake_lever.scale.y = this.scale - 0.1
-
-    brake_lever.y = this.brake_bike.height * 0.45
-    brake_lever.x = this.brake_bike.width * 0.371
-
+    //刹车位置
+    brake_lever.y = 950
+    brake_lever.x = 800
     // 设置锚点
     brake_lever.anchor.x = 1
     brake_lever.anchor.y = 1
-
     // 调整角度
     brake_lever.rotation = (-20 * Math.PI) / 180
-
+    // 将资源都添加到容器中
+    bycyleContainer.addChild(brake_bike)
     bycyleContainer.addChild(brake_lever)
+    bycyleContainer.addChild(brake_handlerbar)
+
+    // 将车容器添加到画布中
+    this.breakBannerContainer.addChild(bycyleContainer)
   }
 
   // 窗口大小发生变化时
-  private resize() {}
+  private _resize() {
+    // 让车一直处于画面右下角
+    const setBycyclePosition = () => {
+      this.breakBannerContainer.x = window.innerWidth - this.breakBannerContainer.width
+      this.breakBannerContainer.y = window.innerHeight - this.breakBannerContainer.height
+    }
+    
+    setBycyclePosition()
+    window.addEventListener("resize", setBycyclePosition)
+  }
 
   mount(selector: string) {
     document.querySelector(selector)?.appendChild(this.app.view)
